c2d05f82a6845acbf0102db54fb29c1f889df3d7
Hack Reactor Student <student@hackreactor.com>
2017-03-29 20:09:02 -0700

    Completed stack and queue for pseudoclassical instantiation. All specs pass for Part 1

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.length = 0;
 };
 
+Queue.prototype.size = function() {
+  return this.length;
+}
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.length] = value;
+  this.length++;
+}
+
+Queue.prototype.dequeue = function() {
+  var tempStorage = {};
+  for (var key in this.storage){
+    if (key > 0){
+     tempStorage[key - 1] = this.storage[key];
+    }
+  }
+
+var finished = this.storage[0];
+  if (this.length > 0){
+  this.length--;
+} 
+
+this.storage = tempStorage;
+  return finished;
+}
