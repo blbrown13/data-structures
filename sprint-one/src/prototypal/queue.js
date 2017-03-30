9dfe6e2b4f6b37ee93024dd45d8c061a1555ef5f
Hack Reactor Student <student@hackreactor.com>
2017-03-29 19:56:54 -0700

    add stack and queue prototypal instantiation

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.length = 0;
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+	this.storage[this.length] = value;
+	this.length++;
+}
 
+queueMethods.dequeue = function() {
+	var tempStorage = {};
+	for (var key in this.storage){
+		if (key > 0){
+			tempStorage[key - 1] = this.storage[key];
+		}
+	}
+
+	var finished = this.storage[0];
+	if (this.length > 0){
+		this.length--;
+	} 
+
+	this.storage = tempStorage;
+	return finished;
+}
+
+queueMethods.size = function() {
+	return this.length;
+}
