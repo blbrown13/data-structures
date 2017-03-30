ee40b356e5aa3c513cdb1699dcb0a95edae00ebc
Hack Reactor Student <student@hackreactor.com>
2017-03-30 15:58:27 -0700

    completed linked list and passed all specs

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,42 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+
+    if (list.head === null) {
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+    }
+
+
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var oldHead = list.head;
+
+    if (list.head.next === null) {
+      list.head = null;
+    } else {
+      list.head = list.head.next;
+    }
+    
+    return oldHead.value;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+
+    var iterate = function(node) {
+      if (node.value === target) {
+        return true;
+      } else {
+        return node.next !== null ? iterate(node.next) : false;
+      }
+    };
+
+    return iterate(currentNode);
   };
 
   return list;
@@ -26,4 +56,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addToTail: O(1) - constant time
+ removeHead: O(1) - constant time
+ contains: O(n) - linear 
  */
